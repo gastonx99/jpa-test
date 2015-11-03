2d9bc98cd6f9e16084fc9b6ecc402656da5b6689
Anders Lundberg <dan.anders.lundberg@server.fake>
2015-11-03 21:16:36 +0100

    Temporarily ignoring managed performed test coz of annoying long execution time

--- src/test/java/se/dandel/test/jpa/ManagedPerformanceTest.java
+++ src/test/java/se/dandel/test/jpa/ManagedPerformanceTest.java
@@ -2,69 +2,71 @@ package se.dandel.test.jpa;
 
 import org.apache.log4j.Logger;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+
+import com.google.inject.Inject;
 
 import se.dandel.test.jpa.dao.DepartmentDAO;
 import se.dandel.test.jpa.junit.GuiceJpaLiquibaseManager;
 
-import com.google.inject.Inject;
-
+@Ignore
 public class ManagedPerformanceTest {
-    private final Logger logger = Logger.getLogger(getClass());
+	private final Logger logger = Logger.getLogger(getClass());
 
-    @Rule
-    @JpaTestConfig
-    public GuiceJpaLiquibaseManager mgr = new GuiceJpaLiquibaseManager();
+	@Rule
+	@JpaTestConfig
+	public GuiceJpaLiquibaseManager mgr = new GuiceJpaLiquibaseManager();
 
-    @Inject
-    private DepartmentDAO departmentDAO;
+	@Inject
+	private DepartmentDAO departmentDAO;
 
-    private long end;
+	private long end;
 
-    private long start;
+	private long start;
 
-    public void createEntities(int c) {
-        start = System.currentTimeMillis();
-        for (int i1 = 1; i1 <= c; i1++) {
-            departmentDAO.create(String.valueOf(i1));
-        }
-        end = System.currentTimeMillis();
-        logger.info("Create took " + (end - start) + " ms");
-    }
+	public void createEntities(int c) {
+		start = System.currentTimeMillis();
+		for (int i1 = 1; i1 <= c; i1++) {
+			departmentDAO.create(String.valueOf(i1));
+		}
+		end = System.currentTimeMillis();
+		logger.info("Create took " + (end - start) + " ms");
+	}
 
-    @Before
-    public void before() {
-        mgr.getEntityManager().createQuery("delete from DepartmentEO").executeUpdate();
-        mgr.getEntityManager().flush();
-    }
+	@Before
+	public void before() {
+		mgr.getEntityManager().createQuery("delete from DepartmentEO").executeUpdate();
+		mgr.getEntityManager().flush();
+	}
 
-    @Test
-    public void unmanaged() {
-        createEntities(10000);
+	@Test
+	public void unmanaged() {
+		createEntities(10000);
 
-        mgr.reset();
+		mgr.reset();
 
-        departmentDAO.findAllReadOnly();
+		departmentDAO.findAllReadOnly();
 
-        start = System.currentTimeMillis();
-        mgr.getEntityManager().flush();
-        end = System.currentTimeMillis();
-        logger.info("Readonly flush took " + (end - start) + " ms");
-    }
+		start = System.currentTimeMillis();
+		mgr.getEntityManager().flush();
+		end = System.currentTimeMillis();
+		logger.info("Readonly flush took " + (end - start) + " ms");
+	}
 
-    @Test
-    public void managed() {
-        createEntities(10000);
+	@Test
+	public void managed() {
+		createEntities(10000);
 
-        mgr.reset();
+		mgr.reset();
 
-        departmentDAO.findAll();
+		departmentDAO.findAll();
 
-        start = System.currentTimeMillis();
-        mgr.getEntityManager().flush();
-        end = System.currentTimeMillis();
-        logger.info("Managed flush took " + (end - start) + " ms");
-    }
+		start = System.currentTimeMillis();
+		mgr.getEntityManager().flush();
+		end = System.currentTimeMillis();
+		logger.info("Managed flush took " + (end - start) + " ms");
+	}
 
 }
